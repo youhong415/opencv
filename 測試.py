d621c9b3cfd520759fa10020ed7c0ee481fd474e
youhong415 <56081287+youhong415@users.noreply.github.com>
2020-01-15 22:05:06 +0800

    Update 測試.py

--- 測試.py
+++ 測試.py
@@ -6,21 +6,20 @@ Created on Wed Jan 15 21:16:43 2020
 """
 
 import cv2
-#read video
+#讀video
 cap = cv2.VideoCapture('C:\\Users\\bbjac\\Git\\opencv\\around.mp4')
 
-
-#define mask position
-upper_left = (280, 100)
-bottom_right = (980, 600)
+upper_left = (280,100)
+bottom_right = (980,600)
 
 
-#define mask
+#一種filter方式
 def sketch_transform(image):
     image_grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image_grayscale_blurred = cv2.GaussianBlur(image_grayscale, (7,7), 0)
     image_canny = cv2.Canny(image_grayscale_blurred, 10, 80)
     _, mask = image_canny_inverted = cv2.threshold(image_canny, 30, 255, cv2.THRESH_BINARY_INV)
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
     return mask
 
 
@@ -29,16 +28,14 @@ while (cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
         #Rectangle marker
-        r = cv2.rectangle(frame, upper_left, bottom_right, (100, 50, 200), 5)
-        rect_img = frame[upper_left[1] : bottom_right[1], upper_left[0] : bottom_right[0]]
-    
+        r = cv2.rectangle(frame, upper_left, bottom_right, (100, 50, 200), 2) # retangle參數:(frame,(左上座標),(右下座標),框的顏色,框的厚度)
+        rect_img = frame[upper_left[1] : bottom_right[1], upper_left[0] : bottom_right[0]] #frame的大小
+
         sketcher_rect = sketch_transform(rect_img)
-        
-        #Conversion for 3 channels to put back on original image (streaming)
-        sketcher_rect_rgb = cv2.cvtColor(sketcher_rect, cv2.COLOR_GRAY2RGB)
+        cv2.imshow('sketcher_rect',sketcher_rect)
     
         #Replacing the sketched image on Region of Interest
-        frame[upper_left[1] : bottom_right[1], upper_left[0] : bottom_right[0]] = sketcher_rect_rgb
+        frame[upper_left[1] : bottom_right[1], upper_left[0] : bottom_right[0]] = sketcher_rect
         
         cv2.imshow('frame',frame)
         
